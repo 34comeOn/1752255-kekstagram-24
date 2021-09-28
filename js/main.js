a13dc5636cd7de5d245ead1dad3ae7083110e99e
34comeOn <mikl.bifg@yandex.ru>
2021-09-28 15:34:58 +0300

    Исправление. Нужно больше фукций

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-// ПЕРВАЯ ФУНКЦИЯ
-
 const getRandomNumber = function (lowNumber, highNumber) {
-  // if (lowNumber < 0 || highNumber < 0) {
-  //   return console.log('Значения диапазона могут быть только положительными');
-  // }
-  // if (lowNumber > highNumber) {
-  //   return console.log('Указаны не верные значения аргумента(ов)');
-  // }
+  if (lowNumber < 0 || highNumber < 0) {
+    return 'Значения диапазона могут быть только положительными';
+  }
+  if (lowNumber > highNumber) {
+    return 'Указаны не верные значения аргумента(ов)';
+  }
 
   const rangeArray = [];
   const range = highNumber - lowNumber;
   for (let index = 0; index <= range; index++) {
     rangeArray[index] = index;
   }
-  // console.log(rangeArray);
   let summary = highNumber + lowNumber;
   if (summary % 2) {
     summary = summary + 1;
@@ -24,27 +21,19 @@ const getRandomNumber = function (lowNumber, highNumber) {
       arrayRandomNumber--;
     }
   }
-  // console.log(arrayRandomNumber);
-  // console.log(rangeArray[arrayRandomNumber]);
-  // const additionalNumber = rangeArray[arrayRandomNumber];
-  // const randomNumber = lowNumber + additionalNumber;
-  // console.log(randomNumber);
+  const additionalNumber = rangeArray[arrayRandomNumber];
+  const randomNumber = lowNumber + additionalNumber;
+  return randomNumber;
 };
 
 getRandomNumber(0, 1);
-
-// ВТОРАЯ ФУНКЦИЯ
 
 const textDescription = document.querySelector('.text__description');
 const textLength = textDescription.textContent.length;
 const MAX_LENGTH = 34;
 
 const getLength = function (stringLength, maximumLength) {
-  if (stringLength <= maximumLength) {
-    return true;
-  }
-
-  return false;
+  return stringLength <= maximumLength;
 };
 
 getLength(textLength, MAX_LENGTH);
