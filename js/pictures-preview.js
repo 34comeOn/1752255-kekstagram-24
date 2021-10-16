4737ab7ca75831d888647144e90bb041dbb911ab
34comeOn <mikl.bifg@yandex.ru>
2021-10-16 14:26:39 +0300

    Отрисуй меня полностью (часть 1). Упростил)

--- js/pictures-preview.js
+++ js/pictures-preview.js
@@ -4,9 +4,7 @@ const picturesContainer = document.querySelector('.pictures');
 const usersPreviewsFragment = document.createDocumentFragment();
 const randomUserTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const usersPreviews = arrayOfObjects;
-
-usersPreviews.forEach(({url, likes, comments}) => {
+arrayOfObjects.forEach(({url, likes, comments}) => {
   const previewElement = randomUserTemplate.cloneNode(true);
   previewElement.querySelector('.picture__img').src = url;
   previewElement.querySelector('.picture__likes').textContent = likes;
